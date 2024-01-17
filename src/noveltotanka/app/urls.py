90853efac6ec75bc3a2ec7466fbfb931772b1daf
matsushita <kokotarom@icloud.com>
2024-01-17 20:46:41 +0900

    ボタン2の追加と1と2の内容調整

--- src/noveltotanka/app/urls.py
+++ src/noveltotanka/app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('run3', views.run3, name='run3'),
+    path('run2', views.run2, name='run2'),
     path('run1', views.run1, name='run1')
 ]
--- src/noveltotanka/app/views.py
+++ src/noveltotanka/app/views.py
@@ -5,10 +5,39 @@ from app.modules import novel_string
 import random
 import re
 
+def search_keyword(string, text):
+    hitted = re.finditer(string, text)
+    result = []
+    for i in hitted:
+        result.append(i)
+    random.shuffle(result)
+    return result[0]
+
 def index(request):
     return render(request, "app/index.html", context=None)
 
 def run1(request):
+    novel_text = novel_string.hayabusa()
+    trimed = novel_string.remove_symbols(novel_text)
+
+    string = 'あなた'
+
+    picked = search_keyword(string, trimed)
+    start = picked.start()
+    end = picked.end()
+
+    tankaText = trimed[start:start+31]
+
+    semitoneStr = 'ぁ|ぃ|ぅ|ぇ|ぉ|ゃ|ゅ|ょ|ァ|ィ|ゥ|ェ|ォ|ャ|ュ|ョ'
+    semitoneCount = len(re.findall(semitoneStr, tankaText))
+
+    tankaText = trimed[start:start+31+semitoneCount]
+
+    context = novel_string.split_five_seven_five(tankaText)
+
+    return render(request, "app/index.html", context=context)
+
+def run2(request):
     novel_text = novel_string.hayabusa()
     trimmed = novel_string.remove_symbols(novel_text)
 
@@ -19,6 +48,7 @@ def run1(request):
     context = novel_string.split_five_seven_five(tankaText)
 
     return render(request, "app/index.html", context=context)
+
 
 def run3(request):
     novel_text = novel_string.hayabusa()
